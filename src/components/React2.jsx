aa2aa987f11f3d1378f444747ea688fd62fa0875
Kailash Rawat <you@example.com>
2023-12-08 19:13:28 +0530

    change

---
+++ src/components/React2.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+function React2() {
+  return (
+    <>
+      Ques1:What is Single Page Application(SPA)?<br /><br />
+
+      ANS1:An SPA (Single-Page Application) is a web app implementation that loads only a single web document, and then updates the body content of that single document via JavaScript APIs such as XMLHttpRequest and Fetch when different content is to be shown.<br /><br />
+
+      Ques2:What is DNS and IP Address?<br /><br />
+
+      Ans2:A DNS (Domain Name System) server is a server that translates domain names (such as www.example.com) into IP addresses. An IP address is a numerical label assigned to each device connected to a computer network that uses the Internet Protocol for communication.<br /><br />
+
+      Ques3:What Is DOM in JS?<br /><br />
+
+      Ans3:The Document Object Model (DOM) is a programming interface for web documents. It representation allows it to be manipulated. As an object-oriented representation of the web page, it can be modified with a scripting language such as JavaScript.<br /><br />
+
+      Ques4:What is  React and why we use React instead of JS?<br /><br />
+
+      Ans4:React is a JavaScript library to build user interface and building UI components. We use React instead of JS because JS used for functionalities not to build single page apllication but React does everyhting to make everyhting poosible for SPA.<br /><br />
+
+      Ques5:What is JSX and why do we use it instead of js?<br /><br />
+
+      Ans5:JSX stands for Java Script Extension and it used in the form of XML. We use it instead of JS because we can write html content in the JSX form whereas JS can't. So this is the reason to we use JSX in react.<br /><br />
+
+      Ques6:What is package.json?<br /><br />
+
+      Ans6:The package. json file contains descriptive and functional metadata about a project, such as a name, version, and dependencies. The file provides the npm package manager with various information to help identify the project and handle dependencies.<br /><br />
+
+      Ques7:What is the Difference between react and react native? Which one is library or framework?<br /><br />
+      
+      Ans7:React is a liary whereas React Native is a framework. React utilizes HTML, CSS, and JavaScript to create interactive user interfaces, whereas React Native utilizes APIs and UI components to build mobile applications. WEcan build single page app with the help of React whereas we can build IOS AND Android app wit the help of react native.<br /><br />
+
+      Ques8:What is difference between class and functional component?
+
+      Ans8:A functional component is just a plain JavaScript pure function that accepts props as an argument and returns a React element(JSX). A class component requires you to extend from React. Component and create a render function that returns a React element.
+
+
+    </>
+  )
+}
+
+export default React2
